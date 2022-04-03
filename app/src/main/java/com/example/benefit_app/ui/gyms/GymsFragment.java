e4badf6623e408ea1e70aa236999f3215ca43c6c
Gabriel <gabriel51coyote@gmail.com>
2022-04-03 17:13:12 -0500

    Gym Fragment - Finder Rough foundation added

--- app/src/main/java/com/example/benefit_app/ui/gyms/GymsFragment.java
+++ app/src/main/java/com/example/benefit_app/ui/gyms/GymsFragment.java
@@ -1,19 +1,77 @@
 package com.example.benefit_app.ui.gyms;
 
+
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
 import com.example.benefit_app.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.Arrays;
+import java.util.List;
 
 
-public class GymsFragment extends Fragment {
+public class GymsFragment extends Fragment implements OnMapReadyCallback {
 
 
+    private MapView mapView;
+    private GoogleMap googleMap;
+
+
+    private PlacesClient placesClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private Location currentLocation;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
+    private static final int DEFAULT_ZOOM = 15;
+    private boolean locationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+    // Used for selecting the current place.
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+    private CameraPosition cameraPosition;
+    private static final int M_MAX_ENTRIES = 5;
+    private String[] likelyPlaceNames;
+    private String[] likelyPlaceAddresses;
+    private List[] likelyPlaceAttributions;
+    private LatLng[] likelyPlaceLatLngs;
 
 
     /* ********************************************************************** */
@@ -21,28 +79,302 @@ public class GymsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    /* ********************************************************************** */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (googleMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, googleMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, currentLocation);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
 
     /* ********************************************************************** */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Setup/Initialize the Places Client SDK
+        if (!Places.isInitialized()) {
+            Places.initialize(getActivity().getApplicationContext(), "AIzaSyCLZuWWw3Rf2Yn5wKRPC2eF9FZGlizVBwA");
+        }
+
+        // Create a new PlacesClient instance
+        placesClient = Places.createClient(getActivity());
+
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        if(savedInstanceState != null){
+            currentLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
+
+
+        /*
+        // Build the map.
+        // [START maps_current_place_map_fragment]
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+
+        */
+
+
     }
 
 
-
     /* ********************************************************************** */
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         /* PURPOSE:             To get our items from the fragment_gyms.xml,
                                 Also return viewer to 'inflate' into the Fragment container viewer */
         View viewer = inflater.inflate(R.layout.fragment_gyms, container, false);
 
-
-
         return viewer;
     }
 
 
+    /* ********************************************************************** */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mapView = view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);//when you already implement OnMapReadyCallback in your fragment
 
 
+
+
+    }
+
+
+    /* ********************************************************************** */
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        googleMap = map;
+
+        getLocationPermission();
+
+        updateLocationUI();
+
+        getDeviceLocation();
+
+        showCurrentPlace();
+
+    }
+
+    /* ********************************************************************** */
+    private void getLocationPermission(){
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    /* ********************************************************************** *//**
+     * Handles the result of the request for location permissions.
+     */
+    // [START maps_current_place_on_request_permissions_result]
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        if (requestCode
+                == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        updateLocationUI();
+    }
+    // [END maps_current_place_on_request_permissions_result]
+
+
+    /* ********************************************************************** */
+    private void updateLocationUI(){
+        if(googleMap == null){
+            return;
+        }
+        try{
+            if(locationPermissionGranted){
+                googleMap.setMyLocationEnabled(true);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                googleMap.setMyLocationEnabled(false);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+                currentLocation = null;
+                getLocationPermission();
+            }
+
+
+        } catch (SecurityException e){
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
+
+    /* ********************************************************************** */
+    private void getDeviceLocation(){
+        try{
+            if (locationPermissionGranted){
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(getActivity(), task -> {
+
+                    if (task.isSuccessful()) {
+                        // Set the map's camera position to the current location of the device.
+                        currentLocation = task.getResult();
+                        if (currentLocation != null) {
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(currentLocation.getLatitude(),
+                                            currentLocation.getLongitude()), DEFAULT_ZOOM));
+                        }
+
+
+                    } else {
+                        Log.d("getDeviceLocation_Tag", "Current location is null. Using defaults.");
+                        Log.e("getDeviceLocation_Tag", "Exception: %s", task.getException());
+                        googleMap.moveCamera(CameraUpdateFactory
+                                .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+                    }
+
+                });
+            }
+
+
+
+        } catch (SecurityException e){
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
+    /* ********************************************************************** */
+    private void showCurrentPlace(){
+        if (googleMap == null) {
+            return;
+        }
+
+        if (locationPermissionGranted) {
+            // Use fields to define the data types to return.
+            List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS,
+                    Place.Field.LAT_LNG);
+
+            // Use the builder to create a FindCurrentPlaceRequest.
+            FindCurrentPlaceRequest request =
+                    FindCurrentPlaceRequest.newInstance(placeFields);
+
+            // Get the likely places - that is, the businesses and other points of interest that
+            // are the best match for the device's current location.
+            @SuppressWarnings("MissingPermission") final
+            Task<FindCurrentPlaceResponse> placeResult =
+                    placesClient.findCurrentPlace(request);
+            placeResult.addOnCompleteListener (task -> {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    FindCurrentPlaceResponse likelyPlaces = task.getResult();
+
+                    // Set the count, handling cases where less than 5 entries are returned.
+                    int count;
+                    if (likelyPlaces.getPlaceLikelihoods().size() < M_MAX_ENTRIES) {
+                        count = likelyPlaces.getPlaceLikelihoods().size();
+                    } else {
+                        count = M_MAX_ENTRIES;
+                    }
+
+                    int i = 0;
+                    likelyPlaceNames = new String[count];
+                    likelyPlaceAddresses = new String[count];
+                    likelyPlaceAttributions = new List[count];
+                    likelyPlaceLatLngs = new LatLng[count];
+
+                    for (PlaceLikelihood placeLikelihood : likelyPlaces.getPlaceLikelihoods()) {
+                        // Build a list of likely places to show the user.
+                        likelyPlaceNames[i] = placeLikelihood.getPlace().getName();
+                        likelyPlaceAddresses[i] = placeLikelihood.getPlace().getAddress();
+                        likelyPlaceAttributions[i] = placeLikelihood.getPlace()
+                                .getAttributions();
+                        likelyPlaceLatLngs[i] = placeLikelihood.getPlace().getLatLng();
+
+                        i++;
+                        if (i > (count - 1)) {
+                            break;
+                        }
+                    }
+
+                    // Show a dialog offering the user the list of likely places, and add a
+                    // marker at the selected place.
+
+                    //MapsActivityCurrentPlace.this.openPlacesDialog();
+                    //TODO: what to do with this ?-> this.openPlacesDialog();
+                }
+                else {
+                    Log.e("showCurrentPlace_tag: ", "Exception: %s", task.getException());
+                }
+            });
+        } else {
+            // The user has not granted permission.
+            Log.i("showCurrentPlace_tag: ", "The user did not grant location permission.");
+
+            // Add a default marker, because the user hasn't selected a place.
+            googleMap.addMarker(new MarkerOptions()
+                    .title("Default Location")
+                    .position(defaultLocation)
+                    .snippet("No places found, because location permission is disabled."));
+
+            // Prompt the user for permission.
+            getLocationPermission();
+        }
+    }
+
+
+    /* ********************************************************************** */
+    private void openPlacesDialog() {
+        // Ask the user to choose the place where they are now.
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // The "which" argument contains the position of the selected item.
+                LatLng markerLatLng = likelyPlaceLatLngs[which];
+                String markerSnippet = likelyPlaceAddresses[which];
+                if (likelyPlaceAttributions[which] != null) {
+                    markerSnippet = markerSnippet + "\n" + likelyPlaceAttributions[which];
+                }
+
+                // Add a marker for the selected place, with an info window
+                // showing information about that place.
+                googleMap.addMarker(new MarkerOptions()
+                        .title(likelyPlaceNames[which])
+                        .position(markerLatLng)
+                        .snippet(markerSnippet));
+
+                // Position the map's camera at the location of the marker.
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerLatLng,
+                        DEFAULT_ZOOM));
+            }
+        };
+
+        // Display the dialog.
+        AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                .setTitle("Choose a Place")
+                .setItems(likelyPlaceNames, listener)
+                .show();
+    }
 }
