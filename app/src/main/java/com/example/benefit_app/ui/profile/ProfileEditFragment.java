de9197011771892206896236cad097c6eb34581e
alexjyoung6 <95151615+alexjyoung6@users.noreply.github.com>
2022-04-05 19:39:09 -0500

    updated

--- app/src/main/java/com/example/benefit_app/ui/profile/ProfileEditFragment.java
+++ app/src/main/java/com/example/benefit_app/ui/profile/ProfileEditFragment.java
@@ -8,7 +8,10 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.benefit_app.R;
 
@@ -16,6 +19,12 @@ public class ProfileEditFragment extends Fragment {
 
 
     private ImageView backButton;
+    private Button finish_edit_button;
+    private TextView first_name_text;
+    private TextView last_name_text;
+    private TextView email_text;
+    private TextView pnum_text;
+    private TextView username_text;
 
     /* ********************************************************************** */
     public ProfileEditFragment() {
@@ -43,6 +52,18 @@ public class ProfileEditFragment extends Fragment {
         backButton.setOnClickListener(view -> getActivity().onBackPressed());
 
 
+        first_name_text = viewer.findViewById(R.id.first_name_text);
+        last_name_text = viewer.findViewById(R.id.last_name_text);
+        email_text = viewer.findViewById(R.id.email_text);
+        pnum_text = viewer.findViewById(R.id.pnum_text);
+        username_text = viewer.findViewById(R.id.username_text);
+        finish_edit_button = viewer.findViewById(R.id.finish_edit_button);
+        finish_edit_button.setOnClickListener(view -> changeInfo());
+
+
+
+
+
         return viewer;
     }
 
@@ -56,6 +77,21 @@ public class ProfileEditFragment extends Fragment {
         transaction.commit();
     }
 
+    private void changeInfo(){
 
+        String fname_input = first_name_text.getText().toString().trim();
+        String lname_input = last_name_text.getText().toString().trim();
+        String email_input = email_text.getText().toString().trim();
+        String pnum_input = pnum_text.getText().toString().trim();
+        String username_input = username_text.getText().toString().trim();
+
+        if(fname_input.isEmpty() || lname_input.isEmpty() || email_input.isEmpty() || pnum_input.isEmpty() || username_input.isEmpty()){
+          alertDialog("Please fill out all entries.");
+        }
+
+    }
+
+    private void alertDialog(String text){ Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+    }
 
 }
