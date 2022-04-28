13cb79b054d2a7fa49219aa54936b74745e11afe
LuisG05 <garcialluis.05@gmail.com>
2022-04-27 23:58:54 -0500

    fixed fitness fragment steps layout

--- app/src/main/java/com/example/benefit_app/ui/fitness/FitnessFragment.java
+++ app/src/main/java/com/example/benefit_app/ui/fitness/FitnessFragment.java
@@ -69,12 +69,12 @@ public class FitnessFragment extends Fragment {
 
 
         // For Water
-        waterProgressBar = viewer.findViewById(R.id.WaterProgressBar);
-        waterProgressBar.setOnClickListener(view -> {loadFragment(MainActivity.fragmentWater);
-            MainActivity.TvSteps.setVisibility(View.INVISIBLE);
-            MainActivity.TvSteps_fractionLine.setVisibility(View.INVISIBLE);
-            MainActivity.TvStepsGoal.setVisibility(View.INVISIBLE);
-            MainActivity.stepsProgress.setVisibility(View.INVISIBLE);});
+//        waterProgressBar = viewer.findViewById(R.id.WaterProgressBar);
+//        waterProgressBar.setOnClickListener(view -> {loadFragment(MainActivity.fragmentWater);
+//            MainActivity.TvSteps.setVisibility(View.INVISIBLE);
+//            MainActivity.TvSteps_fractionLine.setVisibility(View.INVISIBLE);
+//            MainActivity.TvStepsGoal.setVisibility(View.INVISIBLE);
+//            MainActivity.stepsProgress.setVisibility(View.INVISIBLE);});
 
         // Redirects to notifications
         notificationBell = viewer.findViewById(R.id.fitness_notification_bell);
